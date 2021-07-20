a1c35395da22c8b37e4de73188867c2038537bdd
Samuele de Tomasi <el3um4s@gmail.com>
2021-07-20 15:15:55 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Go to: <a sveltekit:prefetch href="/about">About</a></p>
